eb3bba1ac8d77e34da24299fd316710bd70d0337
Miroslav Bauer <mirekys@users.noreply.github.com>
2021-06-08 14:01:47 +0200

    Sync David's mail template fixes

---
+++ core/templates/internalaltmail.php
@@ -0,0 +1,10 @@
+<?php
+print_unescaped($l->t("Dear user,\n\nwe inform you that %s shared %s with you.\nView file here: %s\n\n", array($_['user_displayname'], $_['filename'], $_['link'])));
+if (isset($_['expiration'])) {
+	print_unescaped($l->t("The share will expire on %s.", [$_['expiration']]));
+	print_unescaped("\n\n");
+}
+// TRANSLATORS term at the end of a mail
+p($l->t("Your CESNET Storage Department Team"));
+?>
+<?php print_unescaped($this->inc('plain.mail.footer'));
--- settings/templates/email.new_user.php
+++ settings/templates/email.new_user.php
@@ -1,37 +1,37 @@
 <table cellspacing="0" cellpadding="0" border="0" width="100%">
-    <tr><td>
-            <table cellspacing="0" cellpadding="0" border="0" width="600px">
-                <tr>
-                    <td bgcolor="<?php p($theme->getMailHeaderColor());?>" width="20px">&nbsp;</td>
-                    <td bgcolor="<?php p($theme->getMailHeaderColor());?>">
-                        <img src="<?php p(\OC::$server->getURLGenerator()->getAbsoluteURL(image_path('', 'logo-mail.gif'))); ?>" alt="<?php p($theme->getName()); ?>"/>
-                    </td>
-                </tr>
-                <tr><td colspan="2">&nbsp;</td></tr>
-                <tr>
-                    <td width="20px">&nbsp;</td>
-                    <td style="font-weight:normal; font-size:0.8em; line-height:1.2em; font-family:verdana,'arial',sans;">
+	<tr><td>
+			<table cellspacing="0" cellpadding="0" border="0" width="600px">
+				<tr>
+					<td bgcolor="<?php p($theme->getMailHeaderColor());?>" width="20px">&nbsp;</td>
+					<td bgcolor="<?php p($theme->getMailHeaderColor());?>">
+						<img src="<?php p(\OC::$server->getURLGenerator()->getAbsoluteURL(image_path('', 'logo-mail.gif'))); ?>" alt="<?php p($theme->getName()); ?>"/>
+					</td>
+				</tr>
+				<tr><td colspan="2">&nbsp;</td></tr>
+				<tr>
+					<td width="20px">&nbsp;</td>
+					<td style="font-weight:normal; font-size:0.8em; line-height:1.2em; font-family:verdana,'arial',sans;">
 						<?php
-						print_unescaped($l->t('Dear user,<br><br>your ownCloud account at CESNET Data Storage has been succesfully created.<br><br>Below are listed details about your account.<br><br>Your username: %s<br>Server address: <a href="%s">%s</a><br><a href="https://du.cesnet.cz/en/navody/owncloud/start">User manual</a><br><br>If you have any questions or problems, feel free to contact us at support@cesnet.cz.<br><br>',
+						print_unescaped($l->t('Dear user,<br><br>your ownCloud account at CESNET Data Storage has been succesfully created.<br><br>Details about your account are listed below.<br><br>Your username: %s<br>Server address: <a href="%s">%s</a><br><a href="https://du.cesnet.cz/en/navody/owncloud/start">User manual</a><br><br>If you have any questions or problems, feel free to contact us at support@cesnet.cz.<br><br>',
 							array($_['username'], 'https://owncloud.cesnet.cz', 'https://owncloud.cesnet.cz')));
 
 						// TRANSLATORS term at the end of a mail
-						p($l->t('Your CESNET Storage Department Team'));
+                                                p($l->t('Your CESNET Storage Department Team'));
 						?>
-                    </td>
-                </tr>
-                <tr><td colspan="2">&nbsp;</td></tr>
-                <tr>
-                    <td width="20px">&nbsp;</td>
-                    <td style="font-weight:normal; font-size:0.8em; line-height:1.2em; font-family:verdana,'arial',sans;">--<br>
+					</td>
+				</tr>
+				<tr><td colspan="2">&nbsp;</td></tr>
+				<tr>
+					<td width="20px">&nbsp;</td>
+					<td style="font-weight:normal; font-size:0.8em; line-height:1.2em; font-family:verdana,'arial',sans;">--<br>
 						<?php p($theme->getName()); ?> -
 						<?php p($theme->getSlogan()); ?>
-                        <br><a href="<?php p($theme->getBaseUrl()); ?>"><?php p($theme->getBaseUrl());?></a>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2">&nbsp;</td>
-                </tr>
-            </table>
-        </td></tr>
+						<br><a href="<?php p($theme->getBaseUrl()); ?>"><?php p($theme->getBaseUrl());?></a>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2">&nbsp;</td>
+				</tr>
+			</table>
+		</td></tr>
 </table>
